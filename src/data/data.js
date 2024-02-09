31f43d02e65425cdfe5761249623ad6d10456bd3
mirkete <saenzvalientemirko@gmail.com>
2024-02-08 21:10:47 -0300

    Added Skill Icons - Courses and Projects sections in progress

---
+++ src/data/data.js
@@ -0,0 +1,12 @@
+export const knowledges = [
+	{name: "HTML", color: "rgb(252, 15, 3)", icon:"/html.svg"},
+	{name: "CSS", color: "rgb(23, 96, 233)", icon:"/css.svg"},
+	{name: "Javascript", color: "rgb(242, 242, 68)", icon:"/javascript.svg"},
+	{name: "React", color: "rgb(66, 207, 218)", icon:"/react.svg"},
+	{name: "SQL", color: "rgb(0,94,134)", icon:"/sql.svg"},
+]
+
+export const courses = [
+	{title: "Ingeniería Informatica", institute: "Universidad de Buenos Aires", date: "2022 - Actualidad"},
+	{title: "Bachiller en Ciencias Sociales", institute: 'E.E.S N°1 "Francisco Narciso Laprida"', date: "2017 - 2022"}
+]
