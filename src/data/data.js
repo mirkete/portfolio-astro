40ff787fe44956d298648d04edc79724ae1fa492
mirkete <saenzvalientemirko@gmail.com>
2024-02-14 17:08:02 -0300

    Update Skills section - Minimal design changes

--- src/data/data.js
+++ src/data/data.js
@@ -1,9 +1,15 @@
-export const knowledges = [
+export const frontendSkills = [
 	{name: "HTML", color: "rgb(252, 15, 3)", icon:"/html.svg"},
 	{name: "CSS", color: "rgb(23, 96, 233)", icon:"/css.svg"},
-	{name: "Javascript", color: "rgb(242, 242, 68)", icon:"/javascript.svg"},
+	{name: "Javascript", color: "rgb(247,226,0)", icon:"/javascript.svg"},
 	{name: "React", color: "rgb(66, 207, 218)", icon:"/react.svg"},
+	{name: "Figma", color: "rgb(247,110,95)", icon:"/figma.svg"},
+]
+
+export const backendSkills = [
+	{name: "Node.js", color: "rgb(134,190,0)", icon:"/nodejs.svg"},
 	{name: "SQL", color: "rgb(0,94,134)", icon:"/sql.svg"},
+	{name: "Firebase", color: "rgb(247,162,18)", icon:"/firebase.svg"},
 ]
 
 export const courses = [
@@ -12,6 +18,6 @@ export const courses = [
 ]
 
 export const projects = [
-	{name: "Home Storage", image: "/home-storage.png", url:"https://github.com/mirkete/home-storage", description: "Aplicación web que permite intercambiar archivos mediante red local."},
-	{name: "Promiedos Scrapper", image: "/promiedos.png", url:"https://github.com/mirkete/promiedos-scrapper", description: "Web scrapper del sitio promiedos"},
+	{name: "Home Storage", image: "/home-storage.png", url:"https://github.com/mirkete/home-storage", description: "Aplicación web que permite intercambiar archivos mediante red local.", skills: ["Node.js", "Express", "React"]},
+	{name: "Promiedos Scrapper", image: "/promiedos.png", url:"https://github.com/mirkete/promiedos-scrapper", description: "Web scrapper del sitio promiedos", skills: ["Node.js", "Puppeteer", "Express"]},
 ]
