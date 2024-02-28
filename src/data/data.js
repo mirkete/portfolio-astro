f8dbfdee46f7e5521a74670d45d9f9380d624a81
Mirko Saenz Valiente <saenzvalientemirko@gmail.com>
2024-02-28 16:49:35 -0300

    Design: Changed font and switched to dark mode only

--- src/data/data.js
+++ src/data/data.js
@@ -1,23 +1,23 @@
 export const frontendSkills = [
-	{name: "HTML", color: "rgb(252, 15, 3)", icon:"/html.svg"},
-	{name: "CSS", color: "rgb(23, 96, 233)", icon:"/css.svg"},
-	{name: "Javascript", color: "rgb(247,226,0)", icon:"/javascript.svg"},
-	{name: "React", color: "rgb(66, 207, 218)", icon:"/react.svg"},
-	{name: "Figma", color: "rgb(247,110,95)", icon:"/figma.svg"},
+	{ name: "HTML", color: "rgb(252, 15, 3)", icon: "/html.svg" },
+	{ name: "CSS", color: "rgb(23, 96, 233)", icon: "/css.svg" },
+	{ name: "Javascript", color: "rgb(247,226,0)", icon: "/javascript.svg" },
+	{ name: "React", color: "rgb(66, 207, 218)", icon: "/react.svg" },
+	{ name: "Figma", color: "rgb(247,110,95)", icon: "/figma.svg" },
 ]
 
 export const backendSkills = [
-	{name: "Node.js", color: "rgb(134,190,0)", icon:"/nodejs.svg"},
-	{name: "SQL", color: "rgb(0,94,134)", icon:"/sql.svg"},
-	{name: "Firebase", color: "rgb(247,162,18)", icon:"/firebase.svg"},
+	{ name: "Node.js", color: "rgb(134,190,0)", icon: "/nodejs.svg" },
+	{ name: "SQL", color: "#03A9F4", icon: "/sql.svg" },
+	{ name: "Firebase", color: "rgb(247,162,18)", icon: "/firebase.svg" },
 ]
 
 export const courses = [
-	{title: "Ingeniería Informatica", institute: "Universidad de Buenos Aires", date: "2022 - Actualidad"},
-	{title: "Bachiller en Ciencias Sociales", institute: 'E.E.S N°1 "Francisco Narciso Laprida"', date: "2017 - 2022"}
+	{ title: "Ingeniería Informatica", institute: "Universidad de Buenos Aires", date: "2022 - Actualidad" },
+	{ title: "Bachiller en Ciencias Sociales", institute: 'E.E.S N°1 "Francisco Narciso Laprida"', date: "2017 - 2022" }
 ]
 
 export const projects = [
-	{name: "Home Storage", image: "/home-storage.png", url:"https://github.com/mirkete/home-storage", description: "Aplicación web que permite intercambiar archivos mediante red local.", skills: ["Node.js", "Express", "React"]},
-	{name: "Promiedos Scrapper", image: "/promiedos.png", url:"https://github.com/mirkete/promiedos-scrapper", description: "Web scrapper del sitio promiedos", skills: ["Node.js", "Puppeteer", "Express"]},
+	{ name: "Home Storage", image: "/home-storage.png", url: "https://github.com/mirkete/home-storage", description: "Aplicación web que permite intercambiar archivos mediante red local.", skills: ["Node.js", "Express", "React"] },
+	{ name: "Promiedos Scrapper", image: "/promiedos.png", url: "https://github.com/mirkete/promiedos-scrapper", description: "Web scrapper del sitio promiedos", skills: ["Node.js", "Puppeteer", "Express"] },
 ]
