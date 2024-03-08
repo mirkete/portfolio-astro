ff1ccf24e448672a046b997fe7159f44c7f3e3e2
Mirko Saenz Valiente <saenzvalientemirko@gmail.com>
2024-03-07 21:13:47 -0300

    Add Express and Astro - Orthography corrections

--- src/data/data.js
+++ src/data/data.js
@@ -1,9 +1,10 @@
 export const frontendSkills = [
 	{ name: "HTML", color: "rgb(252, 15, 3)", icon: "/html.svg" },
 	{ name: "CSS", color: "rgb(23, 96, 233)", icon: "/css.svg" },
-	{ name: "Javascript", color: "rgb(247,226,0)", icon: "/javascript.svg" },
+	{ name: "JavaScript", color: "rgb(247,226,0)", icon: "/javascript.svg" },
 	{ name: "React", color: "rgb(66, 207, 218)", icon: "/react.svg" },
 	{ name: "Figma", color: "rgb(247,110,95)", icon: "/figma.svg" },
+	{ name: "Astro", color: "rgb(223, 55, 108)", icon: "/astro.svg" },
 ]
 
 export const backendSkills = [
@@ -11,10 +12,11 @@ export const backendSkills = [
 	{ name: "Firebase", color: "rgb(247,162,18)", icon: "/firebase.svg" },
 	{ name: "SQL", color: "#03A9F4", icon: "/sql.svg" },
 	{ name: "AWS", color: "#FF9900", icon: "/aws.svg" },
+	{ name: "Express", color: "#d4d4d4", icon: "/express.svg" },
 ]
 
 export const courses = [
-	{ title: "Ingeniería Informatica", institute: "Universidad de Buenos Aires", date: "2022 - Actualidad" },
+	{ title: "Ingeniería Informática", institute: "Universidad de Buenos Aires", date: "2022 - Actualidad" },
 	{ title: "Bachiller en Ciencias Sociales", institute: 'E.E.S N°1 "Francisco Narciso Laprida"', date: "2017 - 2022" }
 ]
 
