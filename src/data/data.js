057652117418060411dcb7b1b10e7cf2c1b5c9f5
mirkete <saenzvalientemirko@gmail.com>
2024-04-04 11:16:50 -0300

    Añadir curso y proyecto Mirtemplates - Corregir tamaño de fuente

--- src/data/data.js
+++ src/data/data.js
@@ -3,24 +3,26 @@ export const frontendSkills = [
 	{ name: "CSS", color: "rgb(23, 96, 233)", icon: "/css.svg" },
 	{ name: "JavaScript", color: "rgb(247,226,0)", icon: "/javascript.svg" },
 	{ name: "React", color: "rgb(66, 207, 218)", icon: "/react.svg" },
-	{ name: "Figma", color: "rgb(247,110,95)", icon: "/figma.svg" },
 	{ name: "Astro", color: "rgb(223, 55, 108)", icon: "/astro.svg" },
+	{ name: "Figma", color: "rgb(247,110,95)", icon: "/figma.svg" },
 ]
 
 export const backendSkills = [
 	{ name: "Node.js", color: "rgb(134,190,0)", icon: "/nodejs.svg" },
-	{ name: "Firebase", color: "rgb(247,162,18)", icon: "/firebase.svg" },
-	{ name: "SQL", color: "#03A9F4", icon: "/sql.svg" },
-	{ name: "AWS", color: "#FF9900", icon: "/aws.svg" },
+	{ name: "MySQL", color: "#03A9F4", icon: "/sql.svg" },
 	{ name: "Express", color: "#d4d4d4", icon: "/express.svg" },
+	{ name: "Firebase", color: "rgb(247,162,18)", icon: "/firebase.svg" },
+	{ name: "AWS", color: "#FF9900", icon: "/aws.svg" },
 ]
 
 export const courses = [
 	{ title: "Ingeniería Informática", institute: "Universidad de Buenos Aires", date: "2022 - Actualidad" },
+	{ title: "Itinerario formativo “Asistente en informatica”", institute: 'E.E.S N°1 "Francisco Narciso Laprida"', date: "2020 - 2022" },
 	{ title: "Bachiller en Ciencias Sociales", institute: 'E.E.S N°1 "Francisco Narciso Laprida"', date: "2017 - 2022" }
 ]
 
 export const projects = [
 	{ name: "Home Storage", image: "/home-storage.png", url: "https://github.com/mirkete/home-storage", description: "Aplicación web que permite intercambiar archivos mediante red local.", skills: ["Node.js", "Express", "React"] },
+	{ name: "Mirtemplates", image: "/template-engine.png", url: "https://github.com/mirkete/template-engine-mirko", description: "Mi propio template engine!. Lo cree para aprender sobre el funcionamiento de los template engines. Tambien esta disponible como paquete de npm.", skills: ["Javascript", "Node.js", "npm"] },
 	{ name: "Promiedos Scrapper", image: "/promiedos.png", url: "https://github.com/mirkete/promiedos-scrapper", description: "Web scrapper del sitio promiedos", skills: ["Node.js", "Puppeteer", "Express"] },
 ]
